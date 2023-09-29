eef18a002f66370b7935ebb5d84a46aa402315e9
Svyatoslav Nikolsky <svyatonik@gmail.com>
2023-09-29 10:50:40 +0300

    add RPC required for bridges relay

--- node/src/rpc.rs
+++ node/src/rpc.rs
@@ -8,7 +8,12 @@
 use std::sync::Arc;
 
 use jsonrpsee::RpcModule;
-use polkadot_bulletin_chain_runtime::{opaque::Block, AccountId, Nonce};
+use polkadot_bulletin_chain_runtime::{opaque::Block, AccountId, BlockNumber, Hash, Nonce};
+use sc_consensus_grandpa::{
+	FinalityProofProvider, GrandpaJustificationStream, SharedAuthoritySet, SharedVoterState,
+};
+use sc_consensus_grandpa_rpc::{Grandpa, GrandpaApiServer};
+use sc_rpc::SubscriptionTaskExecutor;
 use sc_transaction_pool_api::TransactionPool;
 use sp_api::ProvideRuntimeApi;
 use sp_block_builder::BlockBuilder;
@@ -17,18 +22,28 @@ use sp_blockchain::{Error as BlockChainError, HeaderBackend, HeaderMetadata};
 pub use sc_rpc_api::DenyUnsafe;
 
 /// Full client dependencies.
-pub struct FullDeps<C, P> {
+pub struct FullDeps<C, P, B> {
 	/// The client instance to use.
 	pub client: Arc<C>,
 	/// Transaction pool instance.
 	pub pool: Arc<P>,
+	/// Subscription task executor.
+	pub subscription_executor: SubscriptionTaskExecutor,
+	/// GRANDPA authority set.
+	pub shared_authority_set: SharedAuthoritySet<Hash, BlockNumber>,
+	/// GRANDPA voter state.
+	pub shared_voter_state: SharedVoterState,
+	/// GRANDPA justifications.
+	pub justification_stream: GrandpaJustificationStream<Block>,
+	/// Finality proof provider.
+	pub finality_proof_provider: Arc<FinalityProofProvider<B, Block>>,
 	/// Whether to deny unsafe calls
 	pub deny_unsafe: DenyUnsafe,
 }
 
 /// Instantiate all full RPC extensions.
-pub fn create_full<C, P>(
-	deps: FullDeps<C, P>,
+pub fn create_full<C, P, B>(
+	deps: FullDeps<C, P, B>,
 ) -> Result<RpcModule<()>, Box<dyn std::error::Error + Send + Sync>>
 where
 	C: ProvideRuntimeApi<Block>,
@@ -37,13 +52,33 @@ where
 	C::Api: substrate_frame_rpc_system::AccountNonceApi<Block, AccountId, Nonce>,
 	C::Api: BlockBuilder<Block>,
 	P: TransactionPool + 'static,
+	B: sc_client_api::Backend<Block> + Send + Sync + 'static,
 {
 	use substrate_frame_rpc_system::{System, SystemApiServer};
 
 	let mut module = RpcModule::new(());
-	let FullDeps { client, pool, deny_unsafe } = deps;
+	let FullDeps {
+		client,
+		pool,
+		subscription_executor,
+		shared_authority_set,
+		shared_voter_state,
+		justification_stream,
+		finality_proof_provider,
+		deny_unsafe,
+	} = deps;
 
 	module.merge(System::new(client.clone(), pool, deny_unsafe).into_rpc())?;
+	module.merge(
+		Grandpa::new(
+			subscription_executor.clone(),
+			shared_authority_set.clone(),
+			shared_voter_state.clone(),
+			justification_stream.clone(),
+			finality_proof_provider.clone(),
+		)
+		.into_rpc(),
+	)?;
 
 	// Extend this RPC with a custom API by using the following syntax.
 	// `YourRpcStruct` should have a reference to a client, which is needed
